cf261ba1b133d49409b2ec7b5a72555c87547ce0
Adam Ali <154936108+AdamAli72@users.noreply.github.com>
2024-02-05 00:07:13 -0500

    got the main functionality of the game, all I have to do now is show he scores

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let points = 0;
+let tally = 0;
 function getComputerChoice(){
     let choice="none";
     let num = Math.floor(Math.random()*3);
@@ -14,18 +16,70 @@ function getComputerChoice(){
 }
 function playRound(playerSelection, computerSelection){
     let message = "";
+    let num = 0;
     if(playerSelection.toUpperCase()=="ROCK" && computerSelection=="Rock"){
-        message="Both of you chose rock, play again";
+        message="Both of you chose Rock, play again";
+        tally=tally+1;
     }
     else if(playerSelection.toUpperCase()=="ROCK" && computerSelection=="Scissors"){
-        message="You Won! Nice job."
+        message="You Won! Nice job.";
+        points = points + 1;
+        tally=tally+1;
     }
-    else{
-        message="You lost :( try again."
+    else if(playerSelection.toUpperCase()=="ROCK" && computerSelection=="Paper"){
+        message="You lost :( try again.";
+        tally=tally+1;
     }
+    else if(playerSelection.toUpperCase()=="SCISSORS" && computerSelection=="Paper"){
+        message="You Won! Nice job.";
+        points = points + 1;
+        tally=tally+1;
+    }
+    else if(playerSelection.toUpperCase()=="SCISSORS" && computerSelection=="Rock"){
+        message="You lost :( try again.";
+        tally=tally+1;
+    }
+    else if(playerSelection.toUpperCase()=="SCISSORS" && computerSelection=="Scissors"){
+        message="Both of you chose Scissors, play again.";
+        tally=tally+1;
+    }
+    else if(playerSelection.toUpperCase()=="PAPER" && computerSelection=="Scissors"){
+        message="You lost :( try again.";
+        tally=tally+1;
+    }
+    else if(playerSelection.toUpperCase()=="PAPER" && computerSelection=="Rock"){
+        message="You Won! Nice job.";
+        points = points + 1;
+        tally=tally+1;
+    }
+    else if(playerSelection.toUpperCase()=="PAPER" && computerSelection=="Paper"){
+        message="Both of you chose Paper, play again";    
+        
+
+    }
+
     return message;
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame(){
+    
+    let userInput = prompt("Please enter your choice (Rock,Paper,Scissors): ");
+    let computerChoice=getComputerChoice();
+    let result=playRound(userInput, computerChoice);
+    if(tally==5){
+        if(points>2){
+            alert("yiou won");
+        }
+        else if(points <2){
+            alert("you lost");
+        }
+        else if(points==2){
+            alert("TIIEEE");
+        }
+    }
+    return result;
+}
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
